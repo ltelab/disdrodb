7a2780eed52d10166ae47cd8f08b7d4ae7917082
KimCandolfi <kim.candolfi@gmail.com>
2021-11-25 15:59:18 +0100

    Changed log path

--- parsiveldb/logger.py
+++ parsiveldb/logger.py
@@ -17,6 +17,6 @@ def log(path):
     
     logger = logging
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename=f'{path}/processed/{file_name}', encoding='utf-8', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' , level=logging.DEBUG)
+    logging.basicConfig(filename=f'{path}/{file_name}', encoding='utf-8', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' , level=logging.DEBUG)
     
     return logger
