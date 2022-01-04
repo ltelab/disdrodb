fcc0e7620aab9b4dabf3f2b919be03fdf63e5f84
KimCandolfi <kim.candolfi@gmail.com>
2022-01-04 12:42:02 +0100

    Edit file

--- disdrodb/readers/EPFL/parser_PAYERNE_2014.py
+++ disdrodb/readers/EPFL/parser_PAYERNE_2014.py
@@ -59,19 +59,17 @@ from disdrodb.logger import close_logger
 
 #-------------------------------------------------------------------------.
 # CLIck Command Line Interface decorator
-# @click.command() # options_metavar='<options>'
-# @click.argument('raw_dir', type=click.Path(exists=True), metavar ='<raw_dir>')
-# @click.argument('processed_dir',                         metavar ='<processed_dir>')  
-# @click.option('-l0',   '--l0_processing',  type=bool, show_default=True, default = True,   help = "Perform L0 processing")
-# @click.option('-l1',   '--l1_processing',  type=bool, show_default=True, default = True,   help = "Perform L1 processing")
-# @click.option('-zarr', '--write_zarr',     type=bool, show_default=True, default = False,  help = "Write L1 to zarr")
-# @click.option('-nc',   '--write_netcdf',   type=bool, show_default=True, default = True,   help = "Write L1 netCDF4")
-# @click.option('-f',    '--force',          type=bool, show_default=True, default = False,  help = "Force overwriting")
-# @click.option('-v',    '--verbose',        type=bool, show_default=True, default = False,  help = "Verbose")
-# @click.option('-d',    '--debugging_mode', type=bool, show_default=True, default = False,  help = "Switch to debugging mode")
-# @click.option('-l',    '--lazy',           type=bool, show_default=True, default = True,   help = "Use dask if lazy=True")
-raw_dir = "/SharedVM/Campagne/ltnas3/Raw/PAYERNE_2014"
-processed_dir = "/SharedVM/Campagne/ltnas3/Processed/PAYERNE_2014"
+@click.command() # options_metavar='<options>'
+@click.argument('raw_dir', type=click.Path(exists=True), metavar ='<raw_dir>')
+@click.argument('processed_dir',                         metavar ='<processed_dir>')  
+@click.option('-l0',   '--l0_processing',  type=bool, show_default=True, default = True,   help = "Perform L0 processing")
+@click.option('-l1',   '--l1_processing',  type=bool, show_default=True, default = True,   help = "Perform L1 processing")
+@click.option('-zarr', '--write_zarr',     type=bool, show_default=True, default = False,  help = "Write L1 to zarr")
+@click.option('-nc',   '--write_netcdf',   type=bool, show_default=True, default = True,   help = "Write L1 netCDF4")
+@click.option('-f',    '--force',          type=bool, show_default=True, default = False,  help = "Force overwriting")
+@click.option('-v',    '--verbose',        type=bool, show_default=True, default = False,  help = "Verbose")
+@click.option('-d',    '--debugging_mode', type=bool, show_default=True, default = False,  help = "Switch to debugging mode")
+@click.option('-l',    '--lazy',           type=bool, show_default=True, default = True,   help = "Use dask if lazy=True")
 
 def main(raw_dir,
          processed_dir, 
@@ -393,14 +391,4 @@ def main(raw_dir,
     
  
 if __name__ == '__main__':
-    main(raw_dir,
-             processed_dir, 
-             l0_processing = True, 
-             l1_processing = True,
-             write_zarr = False,
-             write_netcdf = True,
-             force = True, 
-             verbose = True, 
-             debugging_mode = False, 
-             lazy = True)
-    # main()
+    main()
