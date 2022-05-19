7d0f2e8dd1ace2c381528cc24a9dc06143b06dd2
KimCandolfi <kim.candolfi@gmail.com>
2022-05-19 13:09:02 +0200

    Fix click path

--- disdrodb/readers/EPFL/parser_SAMOYLOV_2017_2019.py
+++ disdrodb/readers/EPFL/parser_SAMOYLOV_2017_2019.py
@@ -61,16 +61,16 @@ from disdrodb.logger import close_logger
 
 # -------------------------------------------------------------------------.
 # CLIck Command Line Interface decorator
-# @click.command()  # options_metavar='<options>'
-# @click.argument('raw_dir', type=click.Path(exists=True), metavar='<raw_dir>')
-# @click.argument('processed_dir', metavar='<processed_dir>')
-# @click.option('-l0', '--l0_processing', type=bool, show_default=True, default=True, help="Perform L0 processing")
-# @click.option('-l1', '--l1_processing', type=bool, show_default=True, default=True, help="Perform L1 processing")
-# @click.option('-nc', '--write_netcdf', type=bool, show_default=True, default=True, help="Write L1 netCDF4")
-# @click.option('-f', '--force', type=bool, show_default=True, default=False, help="Force overwriting")
-# @click.option('-v', '--verbose', type=bool, show_default=True, default=False, help="Verbose")
-# @click.option('-d', '--debugging_mode', type=bool, show_default=True, default=False, help="Switch to debugging mode")
-# @click.option('-l', '--lazy', type=bool, show_default=True, default=True, help="Use dask if lazy=True")
+@click.command()  # options_metavar='<options>'
+@click.argument('raw_dir', type=click.Path(exists=True), metavar='<raw_dir>')
+@click.argument('processed_dir', metavar='<processed_dir>')
+@click.option('-l0', '--l0_processing', type=bool, show_default=True, default=True, help="Perform L0 processing")
+@click.option('-l1', '--l1_processing', type=bool, show_default=True, default=True, help="Perform L1 processing")
+@click.option('-nc', '--write_netcdf', type=bool, show_default=True, default=True, help="Write L1 netCDF4")
+@click.option('-f', '--force', type=bool, show_default=True, default=False, help="Force overwriting")
+@click.option('-v', '--verbose', type=bool, show_default=True, default=False, help="Verbose")
+@click.option('-d', '--debugging_mode', type=bool, show_default=True, default=False, help="Switch to debugging mode")
+@click.option('-l', '--lazy', type=bool, show_default=True, default=True, help="Use dask if lazy=True")
 def main(raw_dir,
          processed_dir,
          l0_processing=True,
@@ -465,14 +465,4 @@ def main(raw_dir,
 
 
 if __name__ == "__main__":
-    # main()
-    main(raw_dir = '/SharedVM/Campagne/EPFL/Raw/SAMOYLOV_2017_2019',
-            processed_dir = '/SharedVM/Campagne/EPFL/Processed/SAMOYLOV_2017_2019',
-            l0_processing=True,
-            l1_processing=False,
-            write_netcdf=False,
-            force=True,
-            verbose=True,
-            debugging_mode=True,
-            lazy=True,
-            )
+    main()
