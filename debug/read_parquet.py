a4651d169b652ab37043191822fa98d0597cf0f1
kim.candolfi <kim.candolfi@gmail.com>
2022-06-13 07:25:04 +0200

    Added temp_parser for Melbourne

--- debug/read_parquet.py
+++ debug/read_parquet.py
@@ -14,42 +14,12 @@ import os
 import numpy as np
 # from disdrodb.io import col_dtype_check
 
-# /SharedVM/Campagne/GPM/processed/MC3E/L0/MC3E_sapu01.parquet
 
-campagna = 'PAYERNE_2014'
-path = f"/SharedVM/Campagne/EPFL/Processed/{campagna}/L0"
-file = campagna + '_s10.parquet'
-file_path = os.path.join(path, file)
 
-file_path = '/SharedVM/Campagne/EPFL/Processed/PLATO_2019/L0/PLATO_2019_s10.parquet'
+file_path = '/home/kimbo/data/Campagne/Processed/MELBURNE/MELBOURNE_2007_THIES/L0/MELBOURNE_2007_THIES_s1.parquet'
 
 df = dd.read_parquet(file_path)
 
 df = df.compute()
 print(df)
 
-campagna = 'HPICONET_2010'
-device = 's12'
-path = f"/SharedVM/Campagne/EPFL/Processed/{campagna}/L0"
-file = campagna + '_' + device + '.parquet'
-parquet_path = os.path.join(path, file)
-# file_path = '/SharedVM/Campagne/DELFT/Processed/TEST_DATA/L0/TEST_DATA_s10.parquet'
-
-df2 = dd.read_parquet(parquet_path)
-
-df2 = df2.compute()
-
-
-
-# df = df.iloc[0:100,:] # df.head(100) 
-# np_arr_str =  df['raw_drop_number'].values.astype(str)
-# list_arr_str = np.char.split(np_arr_str,",")
-# print(len(list_arr_str[0]))
-
-# df2 = df2.iloc[0:100,:] # df.head(100) 
-# np_arr_str2 =  df2['raw_drop_number'].values.astype(str)
-# list_arr_str2 =  np.char.split(np_arr_str2,",")
-# print(len(list_arr_str2[0]))
-
-
-# col_dtype_check(df, path, verbose=True)
