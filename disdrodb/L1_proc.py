697ddc36940c3b495931b5f594dd1ac89c7cd3ad
kim.candolfi <kim.candolfi@gmail.com>
2022-06-14 08:56:24 +0200

    Edit retrieve_L1_raw_arrays's split_str variable to work with MELBOURNE_2007_OTT campaing

--- disdrodb/L1_proc.py
+++ disdrodb/L1_proc.py
@@ -111,6 +111,15 @@ def retrieve_L1_raw_arrays(df, sensor_name, lazy=True, verbose=False):
 
     if sensor_name in ['OTT_Parsivel', 'OTT_Parsivel2']:
         split_str = ','
+        # Found a campaing (MELBOURNE_2007_OTT) with different different divider, this is a temporary solution
+        try:
+            if not df['raw_drop_concentration'].__contains__(split_str):
+                split_str = df['raw_drop_concentration'][0][6]
+        except KeyError:
+            msg = "Something wrong with divider for L1 matrix, default divider is ',', tried to parse with {}".format(split_str)
+            if verbose:
+                print(msg)
+            logger.info(msg)
     if sensor_name in ['Thies_LPM']:
         split_str = ';'
     
