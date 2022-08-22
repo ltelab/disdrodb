abf71a209e2bd3ba48e78b93985ceac6c22658d1
Régis Longchamp <regis@lonco.ch>
2022-08-22 08:51:17 +0200

    feat: add dynamic version from github tag

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "0.0.2"
+VERSION = '{{VERSION_PLACEHOLDER}}'
 DESCRIPTION = "This package provides an API to query, filter and visualize disdrometer data."
 
 
