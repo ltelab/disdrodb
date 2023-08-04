0d4ee4006b2f8547a4e60c1e11e64545ce42a547
Son Pham-Ba <son.phamba@epfl.ch>
2023-08-04 10:14:43 +0200

    doc: switch to "book" sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,12 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
+html_title = "disdrodb"
+html_theme_options = {
+    "repository_url": "https://github.com/ltelab/disdrodb",
+    "use_repository_button": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
