e0c5180264a93e509489fff3c93bce481dc2a0bc
ghiggi <gionata.ghiggi@gmail.com>
2025-07-27 17:48:47 +0200

    Finalized permittivity models module

--- disdrodb/scattering/permittivity.py
+++ disdrodb/scattering/permittivity.py
@@ -16,6 +16,7 @@
 # -----------------------------------------------------------------------------.
 """Implement particle permittivity models."""
 import numpy as np
+import xarray as xr
 
 # Definitions
 # - Complex_refractive_index: m
@@ -134,6 +135,13 @@ def get_refractive_index(temperature, frequency, method):
 #### Liquid Water Refractive Index Models
 
 
+def ensure_array(arr):
+    """Ensure data to be a numpy array or xarray DataArray."""
+    if isinstance(arr, xr.DataArray):
+        return arr
+    return np.asanyarray(arr)
+
+
 def check_temperature_validity_range(temperature, vmin, vmax, method):
     """Check temperature validity range."""
     if np.logical_or(temperature < vmin, temperature > vmax).any():
@@ -148,8 +156,8 @@ def check_frequency_validity_range(frequency, vmin, vmax, method):
     return frequency
 
 
-def get_rain_refractive_index_liebe1991(temperature, frequency):
-    """Compute the complex refractive index according to empirical relationship of Liebe et al. (1991).
+def get_rain_refractive_index_liebe1991_single(temperature, frequency):
+    """Compute the complex refractive index according to the single Debye model of Liebe et al. (1991).
 
     Parameters
     ----------
@@ -176,20 +184,22 @@ def get_rain_refractive_index_liebe1991(temperature, frequency):
     Int. J. Infrared Millim. Waves, 12(7), 659-675.
     https://doi.org/10.1007/BF01008897
     """
-    # Ensure input is numpy array
-    frequency = np.asanyarray(frequency)
-    temperature = np.asanyarray(temperature)
+    # Ensure input is numpy array or xr.DataArray
+    frequency = ensure_array(frequency)
+    temperature = ensure_array(temperature)
 
     # Check frequency and temperature within validity range
-    temperature = check_temperature_validity_range(temperature, vmin=0, vmax=100, method="Liebe1991_2")
-    frequency = check_frequency_validity_range(frequency, vmin=0, vmax=1000, method="Liebe1991_2")
+    temperature = check_temperature_validity_range(temperature, vmin=0, vmax=100, method="Liebe1991single")
+    frequency = check_frequency_validity_range(frequency, vmin=0, vmax=100, method="Liebe1991single")
 
     # Conversion of temperature to Kelvin
     temperature = temperature + 273.15
 
-    # Compute the complex dielectric constant
+    # Compute static dielectric constant (eq. 1)
     theta = 1 - 300 / temperature
     eps_0 = 77.66 - 103.3 * theta
+
+    # Compute the complex dielectric constant (eq. 2)
     eps_1 = 0.066 * eps_0
     gamma_D = 20.27 + 146.5 * theta + 314 * theta**2
     eps = (eps_0 - eps_1) / (1 - 1j * frequency / gamma_D) + eps_1
@@ -199,8 +209,8 @@ def get_rain_refractive_index_liebe1991(temperature, frequency):
     return m
 
 
-def get_rain_refractive_index_liebe1991_2(temperature, frequency):
-    """Compute the complex refractive index according to empirical relationship of Liebe et al. (1991).
+def get_rain_refractive_index_liebe1991(temperature, frequency):
+    """Compute the complex refractive index according to the double Debye model of Liebe et al. (1991).
 
     Parameters
     ----------
@@ -233,33 +243,35 @@ def get_rain_refractive_index_liebe1991_2(temperature, frequency):
     Applied Optics, 11(8), 1836-1844.
     https://doi.org/10.1364/AO.11.001836
     """
-    # Ensure input is numpy array
-    frequency = np.asanyarray(frequency)
-    temperature = np.asanyarray(temperature)
+    # Ensure input is numpy array or xr.DataArray
+    frequency = ensure_array(frequency)
+    temperature = ensure_array(temperature)
 
     # Check frequency and temperature within validity range
-    temperature = check_temperature_validity_range(temperature, vmin=0, vmax=100, method="Liebe1991_2")
-    frequency = check_frequency_validity_range(frequency, vmin=0, vmax=1000, method="Liebe1991_2")
+    temperature = check_temperature_validity_range(temperature, vmin=0, vmax=40, method="Liebe1991")
+    frequency = check_frequency_validity_range(frequency, vmin=0, vmax=1000, method="Liebe1991")
 
     # Conversion of temperature to Kelvin
     temperature = temperature + 273.15
 
-    # Compute the complex dielectric constant
-    Theta = 1 - 300.0 / temperature
-    eps_0 = 77.66 - 103.3 * Theta
+    # Compute static dielectric constant (eq. 1)
+    theta = 1 - 300 / temperature
+    eps_0 = 77.66 - 103.3 * theta
+
+    # Compute the complex dielectric constant (e4, eq5)
     eps_1 = 0.0671 * eps_0
-    eps_2 = 3.52 + 7.52 * Theta
-    Gamma_1 = 20.20 + 146.5 * Theta + 316 * Theta**2
-    Gamma_2 = 39.8 * Gamma_1
+    eps_2 = 3.52 + 7.52 * theta
+    gamma_1 = 20.20 + 146.5 * theta + 316 * theta**2
+    gamma_2 = 39.8 * gamma_1
 
     term1 = eps_0 - eps_1
-    term2 = 1 + (frequency / Gamma_1) ** 2
-    term3 = 1 + (frequency / Gamma_2) ** 2
+    term2 = 1 + (frequency / gamma_1) ** 2
+    term3 = 1 + (frequency / gamma_2) ** 2
     term4 = eps_1 - eps_2
     term5 = eps_2
 
     eps_real = term1 / term2 + term4 / term3 + term5
-    eps_imag = (term1 / term2) * (frequency / Gamma_1) + (term4 / term3) * (frequency / Gamma_2)
+    eps_imag = (term1 / term2) * (frequency / gamma_1) + (term4 / term3) * (frequency / gamma_2)
 
     eps = eps_real + 1j * eps_imag
 
@@ -297,9 +309,9 @@ def get_rain_refractive_index_ellison2005(temperature, frequency):
     J. Phys. Chem. Ref. Data, 36, 1-18.
     https://doi.org/10.1063/1.2360986
     """
-    # Ensure input is numpy array
-    frequency = np.asanyarray(frequency)
-    temperature = np.asanyarray(temperature)
+    # Ensure input is numpy array or xr.DataArray
+    frequency = ensure_array(frequency)
+    temperature = ensure_array(temperature)
 
     # Check frequency and temperature within validity range
     temperature = check_temperature_validity_range(temperature, vmin=0, vmax=100, method="Ellison2005")
@@ -343,82 +355,6 @@ def get_rain_refractive_index_ellison2005(temperature, frequency):
 
 
 def get_rain_refractive_index_turner2016(frequency, temperature):
-    """Compute the complex refractive index according to Turner (2016) model.
-
-    Parameters
-    ----------
-    temperature : array-like
-        Temperature in degree Celsius.
-    frequency : array-like
-        Frequency in GHz.
-
-    Returns
-    -------
-    m : array-like
-        Complex refractive index at requested temperature and frequency.
-
-    Notes
-    -----
-    - The code of this function has been derived from pyDSD Joseph Hardin code available at
-    https://github.com/josephhardinee/PyDSD/blob/main/pydsd/utility/dielectric.py#L36
-
-    References
-    ----------
-    Turner, D.D., S. Kneifel, and M.P. Cadeddu (2016).
-    An improved liquid water absorption model in the microwave for supercooled liquid clouds.
-    J. Atmos. Oceanic Technol., 33(1), 33-44.
-    https://doi.org/10.1175/JTECH-D-15-0074.1.
-    """
-    # Ensure input is numpy array
-    frequency = np.asanyarray(frequency)
-    temperature = np.asanyarray(temperature)
-
-    # Check frequency and temperature within validity range
-    temperature = check_temperature_validity_range(temperature, vmin=-40, vmax=50, method="Turner2016")
-    frequency = check_frequency_validity_range(frequency, vmin=0.5, vmax=500, method="Turner2016")
-
-    # Conversion of frequency to Hz
-    frequency = frequency / 1e-9
-
-    # Compute the complex dielectric constant
-    a = [8.111e01, 2.025]
-    b = [4.434e-3, 1.073e-2]
-    c = [1.302e-13, 1.012e-14]
-    d = [6.627e02, 6.089e02]
-    tc = 1.342e2
-    s = [8.7914e1, -4.044e-1, 9.5873e-4, -1.3280e-6]
-
-    def A_i(i, temperature, frequency):
-        """Get A_i term in double debye model."""
-        delta = a[i] * np.exp(-1 * b[i] * temperature)
-        tau = c[i] * np.exp(d[i] / (temperature + tc))
-
-        return (tau**2 * delta) / (1 + (2 * np.pi * frequency * tau) ** 2)
-
-    def B_i(i, temperature, frequency):
-        """Get B_i term in double debye model."""
-        delta = a[i] * np.exp(-1 * b[i] * temperature)
-        tau = c[i] * np.exp(d[i] / (temperature + tc))
-
-        return (tau * delta) / (1 + (2 * np.pi * frequency * tau) ** 2)
-
-    es = s[0] + s[1] * temperature + s[2] * temperature**2 + s[3] * temperature**3
-
-    eps_real = es - (2 * np.pi * frequency) ** 2 * (A_i(0, temperature, frequency) + A_i(1, temperature, frequency))
-    eps_imag = 2 * np.pi * frequency * (B_i(0, temperature, frequency) + B_i(1, temperature, frequency))
-
-    eps = eps_real + 1j * eps_imag
-
-    # Compute the refractive index
-    m = np.sqrt(eps)
-    return m
-
-
-####----------------------------------------------------------------------------------------
-#### Supercool Liquid Water Refractive Index Models
-
-
-def get_slw_refractive_index_turner2016(temperature, frequency):
     """Compute the complex refractive index using the Turner-Kneifel-Cadeddu (TKC) model.
 
     The TKC supercooled liquid water absorption model was built using both laboratory observations
@@ -443,9 +379,9 @@ def get_slw_refractive_index_turner2016(temperature, frequency):
 
     Notes
     -----
-    - The model is designed to operate only for freshwater (no salinity) over the frequency range from 0.5 to 500 GHz
-    and temperature ranging from -40 degC to +50 degC.
-    - The code of this function has been derived from Davide Ori raincoat code available at
+    - The code of this function has been checked against Joseph Hardin pyDSD and Davide Ori raincoat codes
+    available at:
+    https://github.com/josephhardinee/PyDSD/blob/main/pydsd/utility/dielectric.py#L36
     https://github.com/OPTIMICe-team/raincoat/blob/master/raincoat/scatTable/water.py#L54
 
     References
@@ -461,9 +397,9 @@ def get_slw_refractive_index_turner2016(temperature, frequency):
     J. Appl. Meteor. Climatol., 53, 1028-1045.
     https://doi.org/10.1175/JAMC-D-13-0214.1
     """
-    # Ensure input is numpy array
-    frequency = np.asanyarray(frequency)
-    temperature = np.asanyarray(temperature)
+    # Ensure input is numpy array or xr.DataArray
+    frequency = ensure_array(frequency)
+    temperature = ensure_array(temperature)
 
     # Check frequency and temperature within validity range
     temperature = check_temperature_validity_range(temperature, vmin=-40, vmax=50, method="Turner2016")
@@ -472,50 +408,37 @@ def get_slw_refractive_index_turner2016(temperature, frequency):
     # Conversion of frequency to Hz
     frequency = frequency / 1e-9
 
-    # Empirical coefficients for the TKC model.
-    # - The first 4 are a1, b1, c1, and d1,
-    # - The next four are a2, b2, c2, and d2, and the last one is tc.
-    coef = [8.111e01, 4.434e-03, 1.302e-13, 6.627e02, 2.025e00, 1.073e-02, 1.012e-14, 6.089e02, 1.342e02]
+    # Define coefficients
+    a = [8.111e01, 2.025]
+    b = [4.434e-3, 1.073e-2]
+    c = [1.302e-13, 1.012e-14]
+    d = [6.627e02, 6.089e02]
+    tc = 1.342e2
+    s = [8.79144e1, -4.04399e-1, 9.58726e-4, -1.32802e-6]
 
-    # This helps to understand how things work below
-    a_1 = coef[0]
-    b_1 = coef[1]
-    c_1 = coef[2]
-    d_1 = coef[3]
+    def A_i(i, temperature, frequency):
+        """Compute the relaxation terms A_i (Eq 7) of the double Debye model."""
+        delta = a[i] * np.exp(-1 * b[i] * temperature)  # (Eq 9)
+        tau = c[i] * np.exp(d[i] / (temperature + tc))  # (Eq 10)
 
-    a_2 = coef[4]
-    b_2 = coef[5]
-    c_2 = coef[6]
-    d_2 = coef[7]
+        return (tau**2 * delta) / (1 + (2 * np.pi * frequency * tau) ** 2)  # (Eq 7)
 
-    t_c = coef[8]
+    def B_i(i, temperature, frequency):
+        """Compute the relaxation terms B_i (Eq 7) of the double Debye model."""
+        delta = a[i] * np.exp(-1 * b[i] * temperature)  # (Eq 9)
+        tau = c[i] * np.exp(d[i] / (temperature + tc))  # (Eq 10)
+
+        return (tau * delta) / (1 + (2 * np.pi * frequency * tau) ** 2)  # (Eq 8)
 
     # Compute the static dielectric permittivity (Eq 6)
-    eps_s = 87.9144 - 0.404399 * temperature + 9.58726e-4 * temperature**2.0 - 1.32802e-6 * temperature**3.0
+    es = s[0] + s[1] * temperature + s[2] * temperature**2 + s[3] * temperature**3
 
-    # Compute the components of the relaxation terms (Eqs 9 and 10)
-    # - First Debye component
-    delta_1 = a_1 * np.exp(-b_1 * temperature)
-    tau_1 = c_1 * np.exp(d_1 / (temperature + t_c))
-    # - Second Debye component
-    delta_2 = a_2 * np.exp(-b_2 * temperature)
-    tau_2 = c_2 * np.exp(d_2 / (temperature + t_c))
+    # Compute the complex dielectric constant
+    eps_real = es - (2 * np.pi * frequency) ** 2 * (
+        A_i(0, temperature, frequency) + A_i(1, temperature, frequency)
+    )  # (Eq 4)
+    eps_imag = 2 * np.pi * frequency * (B_i(0, temperature, frequency) + B_i(1, temperature, frequency))  # (Eq 5)
 
-    # Compute the relaxation terms (Eq 7) for the two Debye components
-    term1_p1 = (tau_1**2.0 * delta_1) / (1.0 + (2.0 * np.pi * frequency * tau_1) ** 2.0)
-    term2_p1 = (tau_2**2.0 * delta_2) / (1.0 + (2.0 * np.pi * frequency * tau_2) ** 2.0)
-
-    # Compute the real permittivitity coefficient (Eq 4)
-    eps_real = eps_s - ((2.0 * np.pi * frequency) ** 2.0) * (term1_p1 + term2_p1)
-
-    # Compute the relaxation terms (Eq 8) for the two Debye components
-    term1_p1 = (tau_1 * delta_1) / (1.0 + (2.0 * np.pi * frequency * tau_1) ** 2.0)
-    term2_p1 = (tau_2 * delta_2) / (1.0 + (2.0 * np.pi * frequency * tau_2) ** 2.0)
-
-    # Compute the imaginary permittivitity coefficient (Eq 5)
-    eps_imag = 2.0 * np.pi * frequency * (term1_p1 + term2_p1)
-
-    # Compute the dielectric constant
     eps = eps_real + 1j * eps_imag
 
     # Compute the refractive index
@@ -552,8 +475,7 @@ def get_rayleigh_dielectric_factor(m):
 ####-------------------------------------------------------------------------------------.
 refractive_index_METHODS = {
     "Liebe1991": get_rain_refractive_index_liebe1991,
-    "Liebe1991v2": get_rain_refractive_index_liebe1991_2,
+    "Liebe1991single": get_rain_refractive_index_liebe1991_single,
     "Ellison2005": get_rain_refractive_index_ellison2005,
     "Turner2016": get_rain_refractive_index_turner2016,
-    "Turner2016SLC": get_slw_refractive_index_turner2016,
 }
