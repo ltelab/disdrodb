8beec0e7d4a0d4125f3057f4986b0f1e16542b7d
ghiggi <gionata.ghiggi@gmail.com>
2025-09-16 18:59:52 +0200

    Ensure keeps only second accuracy in dataset time

--- disdrodb/utils/attrs.py
+++ disdrodb/utils/attrs.py
@@ -95,6 +95,7 @@ def update_disdrodb_attrs(ds, product: str):
     # ----------------------------------------------
     # Add time_coverage_start and time_coverage_end
     if "time" in ds.dims:
+        ds["time"] = ds["time"].dt.floor("s")  # ensure no sub-second values
         attrs["time_coverage_start"] = str(ds["time"].data[0])
         attrs["time_coverage_end"] = str(ds["time"].data[-1])
 
